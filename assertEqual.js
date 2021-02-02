cd36b856edbe07db0656e060de48ec5ec8f2f7fc
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-02 01:06:18 +0000

    uploading assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+//function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🔴 " + "Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🟢 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Boot Camp");
+assertEqual("Boon", "Boon");
+assertEqual(1, 1);
+assertEqual(2, 3);
