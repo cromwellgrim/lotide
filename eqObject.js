885421e8b370d6f647b45fbd8afb7987fed71b96
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-05 23:09:44 +0000

    adding new files to lotide project

---
+++ eqObject.js
@@ -0,0 +1,90 @@
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  let returnValue = false;
+  let key;
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    // checks that they are the same length to start
+    for (key in object1) {
+      // going through the index of the object searching for keys
+      const firstValue = object1[key];
+      // saving the value found at each object1 key
+      if (key in object2) {
+        const secondValue = object2[key];
+        // saving the value found at each object2 key
+        if (firstValue === secondValue) {
+          // comparing those saved values
+          returnValue = true;
+        } else {
+          returnValue = false;
+        }
+      } else {
+        returnValue = false;
+      }
+    }
+  };
+  return returnValue
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
+
+// const ab = { a: "1", b: "2" };
+// const ba = { c: "2", a: "1", c: "5" };
+// console.log(eqObjects(ab, ba)); // => true
+
+
+// const printItems = function(array) {
+//   for (let item of array) {
+//     if (Array.isArray(item)) {
+//       printItems(item);
+//     } else {
+//     console.log(item);
+//     }
+//   }
+// }
+
+
+// eqArrays function
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  } return true;
+};
+
+
+// assertArraysEqual
+const assertArraysEqual = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        console.log(`🔴 Assertion Failed: ${array1} !== ${array2}`);
+        return false;
+      }
+    }
+  } else {
+    console.log(`🔴 Assertion Failed: ${array1} !== ${array2}`);
+    return false;
+  }
+  console.log(`🟢 Assertion Passed: ${array1} === ${array2}`);
+  return true;
+};
+
+// assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } return;
+};
---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+  let key;
+  for (k in object) {
+    theValue = object[k];
+    if (value === theValue) {
+      key = k;
+    }
+  }
+  return key
+};
+
+// assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } return;
+};
+
+// test code
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
